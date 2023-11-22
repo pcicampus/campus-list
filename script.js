4faf21f2539cbda500bab33660a382340b2587b0
pcicampus <151060343+pcicampus@users.noreply.github.com>
2023-11-22 11:54:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,78 @@
-var data = {
-    "Africa": {
-        "Botswana": ["Gaborone", "Orapa Lethakane", "Lobatse", "Mathanwane"],
-        "Cameroon": ["Bamenda", "Tombel", "Yaounde", "Manucam", "Douala", "Njombe", "Tiko Manukam", "Molyko Buea Cameroon", "Ebonji Tombel"],
-        "Ghana": ["Gbetsile", "Accra Ghana Spintex", "Takoradi", "Kumasi", "Ashaiman-Zeru", "Adum-Kumasi", "Tema", "Gbawee-Tabora", "Hohoe", "Kotei", "Taabre Kumasi", "Ahwiaa Overseas", "Ayeduase Kumasi", "Navrongo"],
-        "Italy": ["Rovigo", "Musile", "Brescia"],
-        "Kenya": ["Nairobi", "Lodwar", "Mombasa", "Kalawani Mbooni", "Nakuru", "Thika", "Makueni", "Kisii", "Bungoma", "Meru", "Tassia", "Kilifi", "Ngong", "Kasarani", "Kakamega", "CBD", "Juja", "Kitengela"],
-        "Liberia": ["Monrovia"],
-        "Malawi": ["Lilongwe"],
-        "Mozambique": ["Nacala"],
-        "Namibia": ["Windhoek", "Uis", "WalvisBay"],
-        "Togo": ["Lome"],
-        "Uganda": ["Kampala"],
-        "Zambia": ["Lusaka", "Mpika", "Ndola", "Kitwe", "Solwezi", "Livingstone", "Chingola", "Katete", "Mazabuka", "Kaoma", "Kapiri", "Chipata", "Kabwe", "Nakonde"],
-        "Zimbabwe": ["Bulawayo", "Beitbridge", "Masvingo", "Harare"]
+document.addEventListener("DOMContentLoaded", function () {
+  const regionsContainer = document.querySelector('.regions-container');
+  const countriesContainer = document.querySelector('.countries-container');
+  const statesContainer = document.querySelector('.states-container');
+
+  const regionsList = document.querySelector('.regions-list');
+  const countriesList = document.querySelector('.countries-list');
+  const statesList = document.querySelector('.states-list');
+
+  const campusData = {
+    Africa: {
+      Botswana: ['Gaborone', 'Orapa Lethakane', 'Lobatse', 'Mathanwane'],
+      Cameroon: ['Bamenda', 'Tombel', 'Yaounde', 'Manucam', 'Douala', 'Njombe', 'Tiko Manukam', 'Molyko Buea Cameroon', 'Ebonji Tombel'],
+      Ghana: ['Gbetsile', 'Accra Ghana Spintex', 'Takoradi', 'Kumasi', 'Ashaiman-Zeru', 'Adum-Kumasi', 'Tema', 'Gbawee-Tabora', 'Hohoe', 'Kotei', 'Taabre Kumasi', 'Ahwiaa Overseas', 'Ayeduase Kumasi', 'Navrongo'],
+      Kenya: ['Nairobi', 'Lodwar', 'Mombasa', 'Kalawani Mbooni', 'Nakuru', 'Thika', 'Makueni', 'Kisii', 'Bungoma', 'Meru', 'Tassia', 'Kilifi', 'Ngong', 'Kasarani', 'Kakamega', 'CBD', 'Juja', 'Kitengela'],
+      Liberia: ['Monrovia'],
+      Malawi: ['Lilongwe'],
+      Mozambique: ['Nacala'],
+      Namibia: ['Windhoek', 'Uis', 'WalvisBay'],
+      SierraLeone: ['Madina Free Town'],
+      SouthAfrica: ['Polokwane', 'Johannesburg', 'Tembisa', 'Mthatha', 'Paarl', 'Rustenburg', 'Cape Town', 'Pretoria', 'Mabopane', 'Gordon\'s Bay', 'Tshwane Pretoria', 'Atteridgeville', 'Dube Soweto'],
+      Uganda: ['Kampala'],
+      Zambia: ['Lusaka', 'Mpika', 'Ndola', 'Kitwe', 'Solwezi', 'Livingstone', 'Chingola', 'Katete', 'Mazabuka', 'Kaoma', 'Kapiri', 'Chipata', 'Kabwe', 'Nakonde'],
+      Zimbabwe: ['Bulawayo', 'Beitbridge', 'Masvingo', 'Harare'],
     },
-    "Asia": {
-        "Japan": ["Japan"],
-        "Qatar": ["Doha"],
-        "Saudi Arabia": ["Saudi Arabia"],
-        "UAE": ["Dubai"]
+    Europe: {
+      Germany: ['Stuttgart'],
+      Italy: ['Rovigo', 'Musile', 'Brescia'],
+      Jamaica: ['Kingston'],
+      Sweden: ['Orebro'],
+      UnitedKingdom: ['Enfield', 'West London', 'Leeds', 'Croydon', 'Harrow', 'Manchester', 'Berkshire', 'Slough', 'Northampton', 'Birmingham', 'Dublin', 'Shirley', 'North London'],
     },
-    "Europe": {
-        "Germany": ["Stuttgart"],
-        "Sweden": ["Orebro"],
-        "United Kingdom": ["Enfield", "West London", "Leeds", "Croydon", "Harrow", "Manchester", "Berkshire", "Slough", "Northampton", "Birmingham", "Dublin", "Shirley", "North London"]
+    Asia: {
+      Japan: ['Japan'],
+      Qatar: ['Doha'],
+      SaudiArabia: ['Saudi Arabia'],
+      UAE: ['Dubai'],
     },
-    "North America": {
-        "Canada": ["Toronto", "Montreal", "Brampton Mississauga", "Saskatoon", "Winnipeg"],
-        "Jamaica": ["Kingston"],
-        "United States of America": ["Houston", "New York", "Arlington", "Colorado", "Atlanta", "Los Angeles", "Minnesota", "Maryland", "Connecticut", "Florida", "Woodforest", "Oklahoma", "Tampa Bay", "Dallas", "Calgary"]
+    NorthAmerica: {
+      Canada: ['Toronto', 'Montreal', 'Brampton Mississauga', 'Saskatoon', 'Winnipeg'],
+      UnitedStates: ['Houston', 'New York', 'Arlington', 'Colorado', 'Atlanta', 'Los Angeles', 'Minnesota', 'Maryland', 'Connecticut', 'Florida', 'Woodforest', 'Oklahoma', 'Tampa Bay', 'Dallas', 'Calgary'],
     },
-    "Oceania": {
-        "Australia": ["Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide"]
-    },
-    "South America": {
-        "Argentina": ["Buenos Aires", "Cordoba", "Rosario", "Mendoza", "La Plata"]
+  };
+
+  function populateList(list, items) {
+    list.innerHTML = "";
+    items.forEach((item) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = item;
+      list.appendChild(listItem);
+    });
+  }
+
+  function updateCampuses(country) {
+    const campuses = campusData[country] || [];
+    populateList(statesList, campuses);
+  }
+
+  function updateCountries(region) {
+    const countries = Object.keys(campusData[region]);
+    populateList(countriesList, countries);
+    statesList.innerHTML = ""; // Clear states list when updating countries
+  }
+
+  regionsContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('region')) {
+      const selectedRegion = event.target.textContent;
+      updateCountries(selectedRegion);
     }
-};
+  });
 
-var contentDiv = document.getElementById('content');
-
-for (var region in data) {
-    var regionDiv = document.createElement('div');
-    regionDiv.className = 'region';
-
-    var h2 = document.createElement('h2');
-    h2.textContent = region;
-    regionDiv.appendChild(h2);
-
-    var countries = data[region];
-    for (var country in countries) {
-        var countryDiv = document.createElement('div');
-        countryDiv.className = 'country';
-
-        var h3 = document.createElement('h3');
-        h3.textContent = country;
-        countryDiv.appendChild(h3);
-
-        var campuses = countries[country];
-        for (var i = 0; i < campuses.length; i++) {
-            var p = document.createElement('p');
-            p.className = 'campus';
-            p.textContent = campuses[i];
-            countryDiv.appendChild(p);
-        }
-
-        regionDiv.appendChild(countryDiv);
+  countriesContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('country')) {
+      const selectedCountry = event.target.textContent;
+      updateCampuses(selectedCountry);
     }
-
-    contentDiv.appendChild(regionDiv);
-}
+  });
+});
