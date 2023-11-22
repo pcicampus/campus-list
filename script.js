35d5db2bc4f2b801a74471d3252ac6a4246b931f
pcicampus <151060343+pcicampus@users.noreply.github.com>
2023-11-22 16:47:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,92 +1,97 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const regionsContainer = document.querySelector('.regions-container');
-  const countriesContainer = document.querySelector('.countries-container');
-  const statesContainer = document.querySelector('.states-container');
-
-  const countriesList = document.querySelector('.countries-list');
-  const statesList = document.querySelector('.states-list');
-
-  const campusData = {
-    Africa: {
-      Botswana: ['Gaborone', 'Orapa Lethakane', 'Lobatse', 'Mathanwane'],
-      Cameroon: ['Bamenda', 'Tombel', 'Yaounde', 'Manucam', 'Douala', 'Njombe', 'Tiko Manukam', 'Molyko Buea Cameroon', 'Ebonji Tombel'],
-      Ghana: ['Gbetsile', 'Accra Ghana Spintex', 'Takoradi', 'Kumasi', 'Ashaiman-Zeru', 'Adum-Kumasi', 'Tema', 'Gbawee-Tabora', 'Hohoe', 'Kotei', 'Taabre Kumasi', 'Ahwiaa Overseas', 'Ayeduase Kumasi', 'Navrongo'],
-      Kenya: ['Nairobi', 'Lodwar', 'Mombasa', 'Kalawani Mbooni', 'Nakuru', 'Thika', 'Makueni', 'Kisii', 'Bungoma', 'Meru', 'Tassia', 'Kilifi', 'Ngong', 'Kasarani', 'Kakamega', 'CBD', 'Juja', 'Kitengela'],
-      Liberia: ['Monrovia'],
-      Malawi: ['Lilongwe'],
-      Mozambique: ['Nacala'],
-      Namibia: ['Windhoek', 'Uis', 'WalvisBay'],
-      SierraLeone: ['Madina Free Town'],
-      SouthAfrica: ['Polokwane', 'Johannesburg', 'Tembisa', 'Mthatha', 'Paarl', 'Rustenburg', 'Cape Town', 'Pretoria', 'Mabopane', 'Gordon\'s Bay', 'Tshwane Pretoria', 'Atteridgeville', 'Dube Soweto'],
-      Uganda: ['Kampala'],
-      Zambia: ['Lusaka', 'Mpika', 'Ndola', 'Kitwe', 'Solwezi', 'Livingstone', 'Chingola', 'Katete', 'Mazabuka', 'Kaoma', 'Kapiri', 'Chipata', 'Kabwe', 'Nakonde'],
-      Zimbabwe: ['Bulawayo', 'Beitbridge', 'Masvingo', 'Harare'],
+const data = {
+    "Africa": {
+        "Botswana": ["Gaborone", "Orapa Lethakane", "Lobatse", "Mathanwane"],
+        "Cameroon": ["Bamenda", "Tombel", "Yaounde", "Manucam", "Douala", "Njombe", "Tiko Manukam", "Molyko Buea Cameroon", "Ebonji Tombel"],
+        "Ghana": ["Gbetsile", "Accra Ghana Spintex", "Takoradi", "Kumasi", "Ashaiman-Zeru", "Adum-Kumasi", "Tema", "Gbawee-Tabora", "Hohoe", "Kotei", "Taabre Kumasi", "Ahwiaa Overseas", "Ayeduase Kumasi", "Navrongo"],
+        "Liberia": ["Monrovia"],
+        "Malawi": ["Lilongwe"],
+        "Mozambique": ["Nacala"],
+        "Namibia": ["Windhoek", "Uis", "WalvisBay"],
+        "Sierra Leone": ["Madina Free Town"],
+        "South Africa": ["Polokwane", "Johannesburg", "Tembisa", "Mthatha", "Paarl", "Rustenburg", "Cape Town", "Pretoria", "Mabopane", "Gordon's Bay", "Tshwane Pretoria", "Atteridgeville", "Dube Soweto"],
+        "Zambia": ["Lusaka", "Mpika", "Ndola", "Kitwe", "Solwezi", "Livingstone", "Chingola", "Katete", "Mazabuka", "Kaoma", "Kapiri", "Chipata", "Kabwe", "Nakonde"],
+        "Zimbabwe": ["Bulawayo", "Beitbridge", "Masvingo", "Harare"],
     },
-    Europe: {
-      Germany: ['Stuttgart'],
-      Italy: ['Rovigo', 'Musile', 'Brescia'],
-      Jamaica: ['Kingston'],
-      Sweden: ['Orebro'],
-      UnitedKingdom: ['Enfield', 'West London', 'Leeds', 'Croydon', 'Harrow', 'Manchester', 'Berkshire', 'Slough', 'Northampton', 'Birmingham', 'Dublin', 'Shirley', 'North London'],
+    "Asia": {
+        "Japan": ["Japan"],
+        "Qatar": ["Doha"],
+        "Saudi Arabia": ["Saudi Arabia"],
     },
-    Asia: {
-      Japan: ['Japan'],
-      Qatar: ['Doha'],
-      SaudiArabia: ['Saudi Arabia'],
-      UAE: ['Dubai'],
+    "Canada": {
+        "Toronto": ["Toronto"],
+        "Montreal": ["Montreal"],
+        "Brampton Mississauga": ["Brampton Mississauga"],
+        "Saskatoon": ["Saskatoon"],
+        "Winnipeg": ["Winnipeg"],
     },
-    NorthAmerica: {
-      Canada: ['Toronto', 'Montreal', 'Brampton Mississauga', 'Saskatoon', 'Winnipeg'],
-      UnitedStates: ['Houston', 'New York', 'Arlington', 'Colorado', 'Atlanta', 'Los Angeles', 'Minnesota', 'Maryland', 'Connecticut', 'Florida', 'Woodforest', 'Oklahoma', 'Tampa Bay', 'Dallas', 'Calgary'],
+    "Europe": {
+        "Germany": ["Stuttgart"],
+        "Italy": ["Rovigo", "Musile", "Brescia"],
+        "Sweden": ["Orebro"],
+        "United Kingdom": ["Enfield", "West London", "Leeds", "Croydon", "Harrow", "Manchester", "Berkshire", "Slough", "Northampton", "Birmingham", "Dublin", "Shirley", "North London"],
     },
-  };
+    "Middle East": {
+        "UAE": ["Dubai"],
+    },
+    "North America": {
+        "Jamaica": ["Kingston"],
+        "United States of America": ["Houston", "New York", "Arlington", "Colorado", "Atlanta", "Los Angeles", "Minnesota", "Maryland", "Connecticut", "Florida", "Woodforest", "Oklahoma", "Tampa Bay", "Dallas", "Calgary"],
+    },
+    "Oceania": {
+        "Australia": [], // Add cities if applicable
+        "New Zealand": [], // Add cities if applicable
+    },
+    "Sub-Saharan Africa": {
+        "Kenya": ["Nairobi", "Lodwar", "Mombasa", "Kalawani Mbooni", "Nakuru", "Thika", "Makueni", "Kisii", "Bungoma", "Meru", "Tassia", "Kilifi", "Ngong", "Kasarani", "Kakamega", "CBD", "Juja", "Kitengela"],
+        "Uganda": ["Kampala"],
+        "Togo": ["Lome"],
+    },
+};
 
-  function createButton(text, onClick) {
-    const button = document.createElement('button');
-    button.textContent = text;
-    button.addEventListener('click', onClick);
-    return button;
-  }
+document.addEventListener("DOMContentLoaded", () => {
+    const regionsContainer = document.getElementById("regionsContainer");
 
-  function populateList(list, items, onClick) {
-    list.innerHTML = "";
-    items.forEach((item) => {
-      const listItem = document.createElement('li');
-      const button = createButton(item, onClick);
-      listItem.appendChild(button);
-      list.appendChild(listItem);
-    });
-  }
+    for (const region in data) {
+        const regionButton = createButton(region, "region");
+        regionsContainer.appendChild(regionButton);
 
-  function updateCampuses(country) {
-    const campuses = campusData[country] || [];
-    populateList(statesList, campuses);
-  }
+        const countriesContainer = document.createElement("div");
+        countriesContainer.classList.add("container", "hidden");
 
-  function updateCountries(region) {
-    const countries = Object.entries(campusData[region]);
-    populateList(countriesList, countries.map(([country]) => country), () => {
-      // Pass a click handler to display cities when a country is clicked
-      statesList.innerHTML = ""; // Clear states list when updating countries
-    });
-  }
+        for (const country in data[region]) {
+            const countryButton = createButton(country, "country");
+            countriesContainer.appendChild(countryButton);
 
-  function updateCities(city) {
-    // No need to display cities as buttons since there's no further level of nesting
-    statesList.textContent = city;
-  }
+            const citiesContainer = document.createElement("div");
+            citiesContainer.classList.add("container", "hidden");
 
-  regionsContainer.addEventListener('click', function (event) {
-    if (event.target.classList.contains('region')) {
-      const selectedRegion = event.target.textContent;
-      updateCountries(selectedRegion);
+            data[region][country].forEach(city => {
+                const cityButton = createButton(city, "city");
+                citiesContainer.appendChild(cityButton);
+            });
+
+            countriesContainer.appendChild(citiesContainer);
+        }
+
+        regionsContainer.appendChild(countriesContainer);
     }
-  });
 
-  countriesContainer.addEventListener('click', function (event) {
-    if (event.target.classList.contains('country')) {
-      const selectedCountry = event.target.textContent;
-      updateCities(selectedCountry);
-    }
-  });
+    document.querySelectorAll(".region").forEach(regionButton => {
+        regionButton.addEventListener("click", () => {
+            regionButton.nextElementSibling.classList.toggle("hidden");
+        });
+    });
+
+    document.querySelectorAll(".country").forEach(countryButton => {
+        countryButton.addEventListener("click", () => {
+            countryButton.nextElementSibling.classList.toggle("hidden");
+        });
+    });
 });
+
+function createButton(text, type) {
+    const button = document.createElement("button");
+    button.classList.add("button", type);
+    button.innerText = text;
+    return button;
+}
