56334948faa552b86125406993aa2513b69c600f
pcicampus <151060343+pcicampus@users.noreply.github.com>
2023-11-22 13:03:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,92 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const regionsButtonsContainer = document.getElementById('regions-buttons');
-  const countriesButtonsContainer = document.getElementById('countries-buttons');
-  const citiesList = document.getElementById('cities-list');
+  const regionsContainer = document.querySelector('.regions-container');
+  const countriesContainer = document.querySelector('.countries-container');
+  const statesContainer = document.querySelector('.states-container');
+
+  const countriesList = document.querySelector('.countries-list');
+  const statesList = document.querySelector('.states-list');
 
   const campusData = {
     Africa: {
       Botswana: ['Gaborone', 'Orapa Lethakane', 'Lobatse', 'Mathanwane'],
       Cameroon: ['Bamenda', 'Tombel', 'Yaounde', 'Manucam', 'Douala', 'Njombe', 'Tiko Manukam', 'Molyko Buea Cameroon', 'Ebonji Tombel'],
-      // Add more countries...
+      Ghana: ['Gbetsile', 'Accra Ghana Spintex', 'Takoradi', 'Kumasi', 'Ashaiman-Zeru', 'Adum-Kumasi', 'Tema', 'Gbawee-Tabora', 'Hohoe', 'Kotei', 'Taabre Kumasi', 'Ahwiaa Overseas', 'Ayeduase Kumasi', 'Navrongo'],
+      Kenya: ['Nairobi', 'Lodwar', 'Mombasa', 'Kalawani Mbooni', 'Nakuru', 'Thika', 'Makueni', 'Kisii', 'Bungoma', 'Meru', 'Tassia', 'Kilifi', 'Ngong', 'Kasarani', 'Kakamega', 'CBD', 'Juja', 'Kitengela'],
+      Liberia: ['Monrovia'],
+      Malawi: ['Lilongwe'],
+      Mozambique: ['Nacala'],
+      Namibia: ['Windhoek', 'Uis', 'WalvisBay'],
+      SierraLeone: ['Madina Free Town'],
+      SouthAfrica: ['Polokwane', 'Johannesburg', 'Tembisa', 'Mthatha', 'Paarl', 'Rustenburg', 'Cape Town', 'Pretoria', 'Mabopane', 'Gordon\'s Bay', 'Tshwane Pretoria', 'Atteridgeville', 'Dube Soweto'],
+      Uganda: ['Kampala'],
+      Zambia: ['Lusaka', 'Mpika', 'Ndola', 'Kitwe', 'Solwezi', 'Livingstone', 'Chingola', 'Katete', 'Mazabuka', 'Kaoma', 'Kapiri', 'Chipata', 'Kabwe', 'Nakonde'],
+      Zimbabwe: ['Bulawayo', 'Beitbridge', 'Masvingo', 'Harare'],
     },
     Europe: {
       Germany: ['Stuttgart'],
       Italy: ['Rovigo', 'Musile', 'Brescia'],
-      // Add more countries...
+      Jamaica: ['Kingston'],
+      Sweden: ['Orebro'],
+      UnitedKingdom: ['Enfield', 'West London', 'Leeds', 'Croydon', 'Harrow', 'Manchester', 'Berkshire', 'Slough', 'Northampton', 'Birmingham', 'Dublin', 'Shirley', 'North London'],
     },
-    // Add more regions...
+    Asia: {
+      Japan: ['Japan'],
+      Qatar: ['Doha'],
+      SaudiArabia: ['Saudi Arabia'],
+      UAE: ['Dubai'],
+    },
+    NorthAmerica: {
+      Canada: ['Toronto', 'Montreal', 'Brampton Mississauga', 'Saskatoon', 'Winnipeg'],
+      UnitedStates: ['Houston', 'New York', 'Arlington', 'Colorado', 'Atlanta', 'Los Angeles', 'Minnesota', 'Maryland', 'Connecticut', 'Florida', 'Woodforest', 'Oklahoma', 'Tampa Bay', 'Dallas', 'Calgary'],
+    },
   };
 
   function createButton(text, onClick) {
     const button = document.createElement('button');
     button.textContent = text;
-    button.classList.add('button');
     button.addEventListener('click', onClick);
     return button;
   }
 
-  function populateButtons(container, items, onClick) {
-    container.innerHTML = "";
+  function populateList(list, items, onClick) {
+    list.innerHTML = "";
     items.forEach((item) => {
+      const listItem = document.createElement('li');
       const button = createButton(item, onClick);
-      container.appendChild(button);
+      listItem.appendChild(button);
+      list.appendChild(listItem);
     });
+  }
+
+  function updateCampuses(country) {
+    const campuses = campusData[country] || [];
+    populateList(statesList, campuses);
   }
 
   function updateCountries(region) {
-    const countries = Object.keys(campusData[region]);
-    populateButtons(countriesButtonsContainer, countries, () => {
-      citiesList.innerHTML = ""; // Clear cities list when updating countries
+    const countries = Object.entries(campusData[region]);
+    populateList(countriesList, countries.map(([country]) => country), () => {
+      // Pass a click handler to display cities when a country is clicked
+      statesList.innerHTML = ""; // Clear states list when updating countries
     });
   }
 
-  function updateCities(country) {
-    const cities = campusData[regionsButtonsContainer.textContent][country] || [];
-    populateCitiesList(cities);
+  function updateCities(city) {
+    // No need to display cities as buttons since there's no further level of nesting
+    statesList.textContent = city;
   }
 
-  function populateCitiesList(cities) {
-    citiesList.innerHTML = "";
-    cities.forEach((city) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = city;
-      citiesList.appendChild(listItem);
-    });
-  }
+  regionsContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('region')) {
+      const selectedRegion = event.target.textContent;
+      updateCountries(selectedRegion);
+    }
+  });
 
-  function initialize() {
-    const regions = Object.keys(campusData);
-    populateButtons(regionsButtonsContainer, regions, updateCountries);
-  }
-
-  initialize();
+  countriesContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('country')) {
+      const selectedCountry = event.target.textContent;
+      updateCities(selectedCountry);
+    }
+  });
 });
