8ce5e61f001fe14a850631f7172306d01b8498fa
pcicampus <151060343+pcicampus@users.noreply.github.com>
2023-11-23 22:44:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,56 +3,57 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = {
         "Africa": {
             "Botswana": {
-                "Gaborone": {"coordinator": "Mosetsanagape Pheto", "phone": "+26771320770", "address": "123 Main St"},
-                "Orapa Lethakane": {"coordinator": "Onkarabile Mantsane", "phone": "+26774096945 +26772889044", "address": "456 Oak St"},
-                "Lobatse": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Mathanwane": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"}
+                "Gaborone": {"coordinator": "Mosetsanagape Pheto", "phone": "+267 7132 0770", "address": "Cresta Marakanelo - President Hotel Main Hall - The Grand Entrance"},
+                "Orapa Lethakane": {"coordinator": "Onkarabile Mantsane", "phone": "+26774096945 +26772889044", "address": "To be updated soon"},
+                "Lobatse": {"coordinator": "Lebogang Lekgotla", "phone": "+26773165162 +26776555697", "address": "To be updated soon"},
+                "Mathanwane": {"coordinator": "Dziidzani Mooketsi", "phone": "+267 7122 3183", "address": "123 Main St"}
             },
             "Cameroon": {
-                "Bamenda": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Tombel": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Yaounde": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Manucam": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Douala": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Njombe": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Tiko Manukam": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Molyko Buea Cameroon": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Ebonji Tombel": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"}
+                "Bamenda": {"coordinator": "Pastor Kelly Joel", "phone": "+237 67029 3710 +237 65135 5239", "address": "Old Town Opposite Assemblies of God church."},
+                "Tombel": {"coordinator": "Pastor Kome Felix Abwe", "phone": "+237 67778 4643 +237 69626 3164", "address": "South West region "},
+                "Yaounde": {"coordinator": "Pastor Keye Blaise", "phone": "+237654324558 +237672275699", "address": "Biyem Assi Lac, Between Famacie Du Lacand Poubelle"},
+                "Manucam": {"coordinator": "Pastor Dickson Dike", "phone": "+237672230327 +237675488397", "address": "Tiko  Manunkam"},
+                "Douala": {"coordinator": "Pastor Ntoko Rene", "phone": " +237678286711 +237670419532", "address": "Opposite Bocom Bonaberi, Carrefour Nasa City"},
+                "Njombe": {"coordinator": "Jam Remedy Tohmoh", "phone": "+237651924036 +237675915674", "address": "Mboale Carrifou Collar"},
+                "Tiko Manukam": {"coordinator": "Pastor Dickson", "phone": "+237677784643", "address": "To be updated soon"},
+                "Molyko Buea Cameroon": {"coordinator": "Pastor Donald Fon", "phone": "+237 679513863 +237 690672888", "address": "Molyko Beside Deeper Life Church "},
+                "Ebonji Tombel": {"coordinator": "Pastor Diangha Joe", "phone": "+237 679989913 +237 681021898.", "address": "Opposite Grace Liberty Ministry Ebonji."}
             },
             "Ghana": {
-                "Gbetsile": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Accra Ghana Spintex": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Takoradi": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Kumasi": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Ashaiman-Zeru": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Adum-Kumasi": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Tema": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Gbawee-Tabora": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Hohoe": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Kotei": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Taabre Kumasi": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Ahwiaa Overseas": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Ayeduase Kumasi": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
-                "Navrongo": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"}
+                "Gbetsile": {"coordinator": "Pastor Daniel Offei", "phone": "+233 24 328 3267", "address": "Mamens Gardens Goshen Junction, Afienya Road."},
+                "Accra Ghana Spintex": {"coordinator": "Pastor Jerry Owusu Afari", "phone": "+233 26 717 0394  ,  + 233 541367810", "address": "Spintex 18 Junction, Accra Ghana."},
+                "Takoradi": {"coordinator": "Pastor Paul Yankey", "phone": "+233242175530", "address": "Market Circle, Takoradi."},
+                "Kumasi": {"coordinator": "Pastor Mavis", "phone": "+233267587425", "address": "To be updated soon"},
+                "Ashaiman-Zeru": {"coordinator": "Pastor George Yaw Asante", "phone": "+233244083819", "address": "Street Opposite Family Hospital (just by the main Zenu station). "},
+                "Adum-Kumasi": {"coordinator": "Pastor Augustine Adarkwa", "phone": "+233547017407 +233543009286", "address": "Inside CLOGSAC Building near SSNIT house - Adum, Kumasi"},
+                "Tema": {"coordinator": "Pastor Rose Arthur", "phone": "+233244339186, +233239169903", "address": "High Skies College Tema Communityu 7 Adjacent Community 7 Veterinary Hospital"},
+                "Gbawee-Tabora": {"coordinator": "Pastor Matilda Abbey", "phone": "+233268885865", "address": "Tabora Junction House Number 120 Naa Ayorkor Street - Tabora, Accra"},
+                "Hohoe": {"coordinator": "Pastor Francis Afenyo", "phone": "+233 554458134", "address": "Mamaga street, Torkorni, Kpando House"},
+                "Kotei": {"coordinator": "Pastor Bright Adjei", "phone": "+233 553145363, +233 206566936", "address": "Inside Hallowed Hostel KNUST, Behind Kotei R/C Primary School"},
+                "Taabre Kumasi": {"coordinator": "Pastor Edward Owusu", "phone": "+233544956119 +233241932917 +233240628149", "address": "Taabre Off Kumasi-Sunyani Road. (Inside Methodist JHS Building)"},
+                "Ahwiaa Overseas": {"coordinator": "Pastor Emmanuel Atta-Darkwah", "phone": "+2330553949984 +233243559106", "address": "Block A Plot 15, Ahwiaa Overseas Kwabre District, Ashanti, Ghana"},
+                "Ayeduase Kumasi": {"coordinator": "Pastor Richard Kugblenu", "phone": "+233543272482", "address": "Ayeduase Newsite Last Stop, The Nursing and Midwifery Council Building Kumasi"},
+                "Navrongo": {"coordinator": "Pastor Simon Atampugre", "phone": "+233242727470 +233206865643", "address": "Tono Community Nurses Training Junction - Navrongo, Ghana"}
             }
         },
         "Asia": {
             "Japan": {
-                "Japan": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"}
+                "Japan": {"coordinator": "Pastor Narumi Yokochi", "phone": "+81 90-6088-3764", "address": "To be updated soon"}
             },
             "Qatar": {
-                "Doha": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"}
+                "Doha": {"coordinator": "Pastor Augustine Nembo ", "phone": "+97466940952", "address": "To be updated soon"}
             },
             "Saudi Arabia": {
-                "Saudi Arabia": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"}
+                "Saudi Arabia": {"coordinator": "Pastor Bernard Njiru Ireri", "phone": "+1234567890", "address": "To be updated soon"}
             },
             "United Arab Emirates": {
-                "Dubai": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"}
+                "Dubai": {"coordinator": "Pastor Joshua Kato ", "phone": "+971 52 627 4978", "address": "To be updated soon"}
             }
         },
         "North America": {
             "Canada": {
-                "Toronto": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
+                "Scarborough": {"coordinator": "Pastors Sarah Ohenhen and Bro Fabrice Iram", "phone": "+1 (647) 529-8337, +1 (772) 888-0889", "address": "3392 Kingston Road"},
+                "Quebec": {"coordinator": "Sis Justyna Onyobru", "phone": "+1 (438) 220-3865", "address": "42 Rue De La Bourgade"},
                 "Montreal": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
                 "Brampton Mississauga": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
                 "Saskatoon": {"coordinator": "John Doe", "phone": "+1234567890", "address": "123 Main St"},
